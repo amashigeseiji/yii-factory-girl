9f24fff34103e1de9ff25af87739b1d6e5aeb92d
amashigeseiji <tenjuu99@gmail.com>
2016-05-29 19:56:54 +0900

    add FactoryTestCase
    - enable to define factory data declaratively
    
    ```
    class SampleTest extends YiiFactoryGirl\FactoryTestCase {
        protected $factories = [
          'sample1' => ['SampleModel', ['name' => 'test']]
        ];
    
        public function testSample() {
            $this->assertInstanceOf('SampleModel', $this->sample1);
            $this->assertEquals('test', $this->sample1->name);
        }
    }
    ```

---
+++ src/YiiFactoryGirl/FactoryTestCase.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * FactoryTestCase
+ *
+ * DESCRIPTION:
+ *  If protected property "$factories" is defined,
+ *  you can get AR instance with "$this->{:$name}"
+ *
+ * SAMPLE:
+ * ```
+ * <?php
+ *  use YiiFactoryGirl\FactoryTestCase;
+ *  class HogeTest case extends FactoryTestCase
+ *  {
+ *      protected $factory = array(
+ *          'User1' => array('User'),
+ *          'User2' => array('User', array('name' => 'testname'), 'registered')
+ *      )
+ *
+ *      public function testIsUser()
+ *      {
+ *          // you can get AR instance by $factory key,
+ *          // and this is recorded by FactoryGirl.
+ *          $this->assertEquals($this->User1->id, User::findByPk($this->User1->id));
+ *      }
+ *
+ *      // you can override factory method with following format.
+ *      // function {:ModelName}Factory() {}
+ *      public function UserFactory($args = array(), $alias = null)
+ *      {
+ *          // Do handle arguments or create some required data
+ *          return parent::UserFactory();
+ *      }
+ *  }
+ * ```
+ *
+ * @author Seiji Amashige <tenjuu99@gmail.com>
+ * @package YiiFactoryGirl
+ */
+
+namespace YiiFactoryGirl;
+
+abstract class FactoryTestCase extends \CTestCase
+{
+    /**
+     * @var $factories
+     * Definition of factory data.
+     *
+     * Define $factories in sub-classes like this:
+     * ```
+     *   $factories = array(
+     *       'user1' => array('User', array('name' => 'test'), 'default'),
+     *       'user2' => 'User',
+     *       'user3' => array('User', array('relations' => array('Identity'))) // definition of relation
+     *   )
+     * ```
+     *
+     * You can get AR instance in testcase:
+     * ```
+     *   $this->user1;
+     *   $this->user2;
+     *   $this->user3->Identity;
+     * ```
+     */
+    protected $factories = array();
+
+    /**
+     * @var $repository
+     * Cache instance of factorized record.
+     */
+    private $repository = array();
+
+    /**
+     * __get
+     *
+     * If given $name is defined in $factories as key,
+     * create AR record and cache in $repository.
+     *
+     * @param string $name property name
+     * @return CActiveRecord
+     * @throws YiiFactoryGirl\FactoryException
+     */
+    public function __get($name)
+    {
+        if (!empty($this->factories) && isset($this->factories[$name])) {
+            if (!isset($this->repository[$name])) {
+                $args = $this->factories[$name];
+                if (is_string($args)) {
+                    $class = $args;
+                    $args = array();
+                } elseif (is_array($args)) {
+                    $class = array_shift($args);
+                } else {
+                    throw new FactoryException('$factories[' . $name . '] is invalid definition.');
+                }
+                $method = $class . FactoryGirl::FACTORY_METHOD_SUFFIX;
+                $this->repository[$name] = call_user_func_array(array($this, $method), $args);
+            }
+
+            return $this->repository[$name];
+        }
+
+        throw new FactoryException("Unknown property '{$name}' for class '". get_class($this) ."'.");
+    }
+
+    /**
+     * __call
+     *
+     * @param string $name method name
+     * @param array $args
+     * @return mixed
+     * @throws YiiFactoryGirl\FactoryException
+     */
+    public function __call($name, $args)
+    {
+        if (FactoryGirl::isCallable($name)) {
+            return call_user_func_array(array('YiiFactoryGirl\FactoryGirl', $name), $args);
+        }
+
+        throw new FactoryException('Call to undefined method ' . get_class($this) . "::{$name}().");
+    }
+
+    /**
+     * truncateTable
+     *
+     * If record is deleted, $repository's ActiveRecord will be inconsistent.
+     *
+     * @param string $tableName table name
+     * @return void
+     */
+    protected function truncateTable($tableName)
+    {
+        FactoryGirl::truncateTable($tableName);
+        foreach ($this->repository as $key => $record) {
+            if ($record->tableName() === $tableName) {
+                unset($this->repository[$key]);
+            }
+        }
+    }
+}
---
+++ tests/YiiFactoryGirl/FactoryTestCaseTest.php
@@ -0,0 +1,148 @@
+<?php
+
+use YiiFactoryGirl\FactoryTestCase;
+
+/**
+ * @coversDefaultClass YiiFactoryGirl\FactoryTestCase
+ */
+class FactoryTestCaseTest extends FactoryTestCase
+{
+    protected $factories = array(
+        'noRelation1'    => 'HaveNoRelation',
+        'noRelation2'    => array('HaveNoRelation', array('name' => 'hoge')),
+        'author1'    => 'Author',
+        'book1'    => array('Book', array('Author' => 'author1')),
+        'notExist' => array('NotExist'),
+        'author2'    => array('Author', array(
+            'relations' => array('Books')
+        )),
+        'publisher' => array('Publisher', array(
+            'name' => 'Iwanami shoten publishers',
+            'relations' => array(
+                'Series' => array(
+                    array(
+                        'name' => 'Iwanami library',
+                        'relations' => array('Books' => array(
+                            array('name' => 'The letters of Vincent Van Gogh'),
+                            array('name' => 'The Critic of Pure Reason'),
+                            array('name' => 'Discourse on Method')
+                        ))
+                    ),
+                    array(
+                        'name' => 'Iwanami science library',
+                        'relations' => array('Books' => array(
+                            array('name' => 'What is the "Elements" by Euclid?'),
+                            array('name' => 'Why does Human draw a picture?'),
+                        ))
+                    ),
+                )
+            )
+        ))
+    );
+
+    /**
+     * setUpBeforeClass
+     *
+     * Migrate
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        Yii::app()->migrate->up();
+    }
+
+    /**
+     * tearDownAfterClass
+     *
+     * Migrate down
+     * @return void
+     */
+    public static function tearDownAfterClass()
+    {
+        Yii::app()->migrate->down();
+    }
+
+    /**
+     * @covers ::__get
+     */
+    public function testFactoryTestCase()
+    {
+        $this->assertNotNull(HaveNoRelation::model()->findByPk($this->noRelation1->id));
+        $this->assertNotNull(HaveNoRelation::model()->findByPk($this->noRelation2->id));
+        $this->assertEquals('hoge', $this->noRelation2->name);
+        // Is ActiveRecord cached in FactoryTestCase::$repository?
+        $this->assertEquals($this->noRelation1->id, $this->noRelation1->id);
+    }
+
+    /**
+     * override test
+     */
+    public function BookFactory($args = array(), $alias = null)
+    {
+        if (isset($args['Author'])) {
+            $authorArg = $args['Author'];
+            unset($args['Author']);
+            $author = is_string($authorArg) ? $this->$authorArg : parent::AuthorFactory($authorArg);
+            $args['Author_id'] = $author->id;
+        }
+
+        return parent::BookFactory($args, $alias);
+    }
+
+    /**
+     * override test
+     */
+    public function NotExistFactory()
+    {
+        return __METHOD__;
+    }
+
+    /**
+     * @covers ::__call
+     */
+    public function testOverriddenFactory()
+    {
+        $this->assertEquals($this->author1->id, $this->book1->Author->id);
+        $this->assertEquals('FactoryTestCaseTest::NotExistFactory', $this->notExist);
+    }
+
+    /**
+     * @covers ::truncateTable
+     */
+    public function testTruncateTable()
+    {
+        $noRelation1 = $this->noRelation1;
+        $this->truncateTable('HaveNoRelation');
+        // Is FactoryTestCase::$repository['noRelation1'] cleared?
+        $this->assertNotEquals($noRelation1->id, $this->noRelation1->id);
+        $this->assertNull(HaveNoRelation::model()->findByPk($noRelation1->id));
+    }
+
+    /**
+     * @covers ::__get
+     */
+    public function testRelations()
+    {
+        $this->assertInstanceOf('Book', $this->author2->Books[0]);
+        foreach ($this->publisher->Series as $series) {
+            if ($series->name === 'Iwanami library') {
+                $this->assertCount(3, $series->Books);
+                $bookNames = array_map(function(Book $book) {
+                    return $book->name;
+                }, $series->Books);
+                $this->assertContains('The letters of Vincent Van Gogh', $bookNames);
+                $this->assertContains('The Critic of Pure Reason', $bookNames);
+                $this->assertContains('Discourse on Method', $bookNames);
+            } elseif ($series->name === 'Iwanami science library') {
+                $this->assertCount(2, $series->Books);
+                $bookNames = array_map(function(Book $book) {
+                    return $book->name;
+                }, $series->Books);
+                $this->assertContains('What is the "Elements" by Euclid?', $bookNames);
+                $this->assertContains('Why does Human draw a picture?', $bookNames);
+            } else {
+                $this->fail('invalid series name');
+            }
+        }
+    }
+}
