7c1eccc12a66e0b880434881cd188cfcd9dd7b20
amashigeseiji <tenjuu99@gmail.com>
2016-06-09 02:34:58 +0900

    add YiiFactoryGirl\Factory test

--- tests/mock/models/HaveNoRelation.php
+++ tests/mock/models/HaveNoRelation.php
@@ -1,6 +1,12 @@
 <?php
 class HaveNoRelation extends CActiveRecord
 {
+    public static $staticProperty = null;
+
+    public $publicProperty = null;
+
+    private $privateProperty = null;
+
     public static function model($className = __CLASS__)
     {
         return parent::model($className);
